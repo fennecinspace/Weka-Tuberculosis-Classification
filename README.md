8b889c2014b62dc80bb25ab96917b93719a41825
Mohamed Benkedadra <hammicristo@gmail.com>
2018-11-08 21:07:23 +0100

    added bagging

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ for help:
 
 
 ### Dev:
-`Run 0043 => Mesure 0.563 - Params   -C 0.11998759910482926 -Q 85 -O -A -J -doNotMakeSplitPointActualValue`
+> Run 0015 => ROC Area 0.621 <br>
+> Bagging Parameters :  -P 55 -S 29 -I 4  <br>
+> Algorithme Parameters :  -C 0.8669210380528916 -O -B -S -J  <br>
 
     python3 learn.py -d $HOME"/Desktop/Machine Learning - M1 ISI/tp-projet/data/features/train/y_features_opt2_300_train_MODIFIED.csv" -o $HOME/Desktop/learn.json -p /opt/weka/weka.jar -c "weka.classifiers.trees.J48"
+
+    $HOME/Desktop/app_machine/tp-projet/data/features/train/y_features_opt2_300_train.csv
--- learn.py
+++ learn.py
@@ -5,6 +5,9 @@ import logging as lg
 
 DEFAULT_ALGO_CLASS = "weka.classifiers.trees.J48"
 DEFAULT_WEKA_PATH = "/opt/weka/weka.jar"
+BAGGING = " weka.classifiers.meta.Bagging"
+DEFAULT_MESURE = 'ROC Area'
+DONE_J48_PARAMS, DONE_BAGGING_PARAMS = [], []
 
 def get_args(argv):
     data, output, weka_path, weka_class = "", "", "", ""
@@ -32,11 +35,12 @@ def get_args(argv):
 
 
 class Weka():
-    __slots__ = ['weka_path', 'weka_class'] 
+    __slots__ = ['weka_path', 'weka_class', 'bagging_enabled'] 
     
-    def __init__(self, weka_path = None, weka_class = None):
+    def __init__(self, weka_path = None, weka_class = None, bagging_enabled = False):
         self.weka_path = self.check_weka_path(weka_path or DEFAULT_WEKA_PATH)
         self.weka_class = weka_class or DEFAULT_ALGO_CLASS
+        self.bagging_enabled = bagging_enabled
 
     @classmethod
     def check_weka_path(cls, path):
@@ -50,7 +54,7 @@ class Model():
 
     def __init__(self, data_path, output_path):
         self.data_path = self.check_data_path(data_path)
-        self.output_path = self.check_output_path(output_path)
+        # self.output_path = self.check_output_path(output_path)
 
     @classmethod
     def check_output_path(cls, path):
@@ -75,75 +79,142 @@ class Model():
             avgs_str = [line for line in data.splitlines() if line.find('Weighted Avg') != -1][0].strip()
 
             columns = re.findall('(\w+[ -]\w+|\w+)', columns_str)[:-1]
-            avgs = re.findall('\d+\.\d+|\?', avgs_str)
-            avgs = [x if x != '?' else -1 for x in avgs]
+            avgs_raw = re.findall('\d+\.\d+|\?', avgs_str)
+            avgs = [x if x != '?' else -1 for x in avgs_raw]
             if columns.index(col) != -1:
-                return float(avgs[columns.index(col)]), data
+                return float(avgs[columns.index(col)]), data, columns, avgs_raw
             else:
                 print('Column ({}) could not be found !'.format(col))
-                return 0, data
+                return 0, data, [], []
         except:
-            return 0, all_data
+            return 0, all_data, [], []
+
+    @classmethod
+    def get_main_command(cls, weka):
+        ## setting up main command
+        command = "java -cp {}".format(weka.weka_path)
+        if weka.bagging_enabled:
+            command = "{} {}".format(command, BAGGING)
+        else:
+            command = "{} {}".format(command, weka.weka_class)
+        return command
 
 
-    def learn(self, command):
+    def learn(self, weka):
         max_mesure = 0
-        best_params = ''
+        best_algo_params = ''
+        best_bagging_params = ''
+        best_mesures = [[],[]]
         i = 0
+        
+        command = self.get_main_command(weka)
+
         while True:
             i += 1
-            params = self.get_j48_params()
-            final_command = "{} {} '{}' {}".format(command, "-t", self.data_path, params)
+            algo_params = self.get_j48_params()
+
+            if weka.bagging_enabled:
+                bagging_params = self.get_bagging_params()
+                final_command = "{} {} -t '{}' -W {} -- {}".format(command, bagging_params, self.data_path, weka.weka_class, algo_params)
+            else:
+                final_command = "{} {} -t '{}'".format(command, algo_params, self.data_path)
+
             # print(params)
             # print(final_command)
             try:
                 returned_raw = sp.check_output(['bash', '-c', final_command], stderr = sp.STDOUT)
-                mesure, returned = self.get_weka_learning_result(returned_raw, 'ROC Area')
+                mesure, returned, mesures_cols, mesures_vals = self.get_weka_learning_result(returned_raw, DEFAULT_MESURE)
                 if mesure > max_mesure:
                     max_mesure = mesure
-                    best_params = params
+                    best_algo_params = algo_params
+                    
+                    if weka.bagging_enabled:
+                        best_bagging_params = bagging_params
+                    best_mesures = [mesures_cols, mesures_vals]
 
                 os.system('clear')
-                sys.stdout.write('\rRun {} => Mesure {} - Params  {}'.format(str(i).zfill(4), max_mesure, best_params))
+                print('Run {} => {} {}'.format(str(i).zfill(4), DEFAULT_MESURE, max_mesure), end = '\n\n')
+                if weka.bagging_enabled:
+                    print('Bagging Parameters : {}'.format(best_bagging_params))
+                print('Algorithme Parameters : {}'.format(best_algo_params), end = '\n\n')
+                print('Mesures :')
+                for col, val in list(zip(best_mesures[0], best_mesures[1])):
+                    print('{} : {}'.format(col, val))
 
             except Exception as e:
                 lg.exception(e)
 
     def get_j48_params(self):
+        global DONE_J48_PARAMS
         params = ''
-        if bool(random.getrandbits(1)): 
-            params = '{} -C {}'.format(params, random.uniform(0,1)) ## above .5 is same as disabling it
-        else:
-            if bool(random.getrandbits(1)): 
-                params = '{} -N {} -R'.format(params, random.randint(2, 130)) # replace 130 with number of example maybe ? ask teacher for limit
-            else: 
-                params = '{} -U'.format(params)
-
-        if bool(random.getrandbits(1)): 
-            params = '{} -M {}'.format(params, random.randint(1, 130)) # replace 130 with number of example maybe ? ask teacher for limit
-
-        if bool(random.getrandbits(1)): 
-            params = '{} -Q {}'.format(params, random.randint(1,130)) # replace 130 with number of example maybe ? ask teacher for limit
-
-        if bool(random.getrandbits(1)): 
-            params = '{} -O'.format(params)
-
-        if bool(random.getrandbits(1)): 
-            params = '{} -B'.format(params)
-
-        if params.find('-U') == -1:
-            if bool(random.getrandbits(1)): 
-                params = '{} -S'.format(params)
-            if bool(random.getrandbits(1)): 
-                params = '{} -L'.format(params)
-            if bool(random.getrandbits(1)): 
-                params = '{} -A'.format(params)
-            if bool(random.getrandbits(1)): 
-                params = '{} -J'.format(params)
         
-        if bool(random.getrandbits(1)): 
-            params = '{} -doNotMakeSplitPointActualValue'.format(params)
+        while params not in DONE_J48_PARAMS and params == '':
+            if bool(random.getrandbits(1)): 
+                params = '{} -C {}'.format(params, random.uniform(0,1)) ## above .5 is same as disabling it
+            else:
+                if bool(random.getrandbits(1)): 
+                    params = '{} -N {} -R'.format(params, random.randint(2, 130)) # replace 130 with number of example maybe ? ask teacher for limit
+                else: 
+                    params = '{} -U'.format(params)
 
+            if bool(random.getrandbits(1)): 
+                params = '{} -M {}'.format(params, random.randint(1, 130)) # replace 130 with number of example maybe ? ask teacher for limit
+
+            if bool(random.getrandbits(1)): 
+                params = '{} -Q {}'.format(params, random.randint(1,130)) # replace 130 with number of example maybe ? ask teacher for limit
+
+            if bool(random.getrandbits(1)): 
+                params = '{} -O'.format(params)
+
+            if bool(random.getrandbits(1)): 
+                params = '{} -B'.format(params)
+
+            if params.find('-U') == -1:
+                if bool(random.getrandbits(1)): 
+                    params = '{} -S'.format(params)
+                if bool(random.getrandbits(1)): 
+                    params = '{} -L'.format(params)
+                if bool(random.getrandbits(1)): 
+                    params = '{} -A'.format(params)
+                if bool(random.getrandbits(1)): 
+                    params = '{} -J'.format(params)
+            
+            if bool(random.getrandbits(1)): 
+                params = '{} -doNotMakeSplitPointActualValue'.format(params)
+
+            if params == '':
+                params = '-C 0.25 -M 7'
+
+        DONE_J48_PARAMS += [params]
+
+        return params
+
+    def get_bagging_params(self):
+        global DONE_BAGGING_PARAMS
+        params = ''
+
+        while params not in DONE_BAGGING_PARAMS and params == '':
+            if bool(random.getrandbits(1)): 
+                params = "{} -P {}".format(params, random.randint(0, 100))
+            if bool(random.getrandbits(1)): 
+                params = "{} -O".format(params)
+            if bool(random.getrandbits(1)): 
+                params = "{} -S {}".format(params, random.randint(1, 130))
+            if bool(random.getrandbits(1)): 
+                params = "{} -num-slots {}".format(params, random.randint(1, 30))
+            if bool(random.getrandbits(1)): 
+                params = "{} -I {}".format(params, random.randint(1, 30))
+            if bool(random.getrandbits(1)): 
+                params = "{} -store-out-of-bag-predictions".format(params)
+            if bool(random.getrandbits(1)): 
+                params = "{} -output-out-of-bag-complexity-statistics".format(params)
+            if bool(random.getrandbits(1)): 
+                params = "{} -represent-copies-using-weights".format(params)
+                        
+            if params == '':
+                params = '-S 50'
+
+        DONE_BAGGING_PARAMS += [params]
         return params
 
 
@@ -152,16 +223,8 @@ if __name__ == '__main__':
     data, output, weka_path, weka_class = get_args(sys.argv[1:])
 
     ## intializing classes
-    weka = Weka(weka_path, weka_class)
+    weka = Weka(weka_path, weka_class, True)
     model = Model(data, output)
 
-    ## setting up main command
-    command = "java -cp {} {}".format(weka.weka_path, weka.weka_class)
-
     ## learning
-    model.learn(command)
-
-    
-# python3 learn.py -d $HOME"/Desktop/Machine Learning - M1 ISI/tp-projet/data/features/train/y_features_opt2_300_train_MODIFIED.csv" -o $HOME/Desktop/learn.json -p /opt/weka/weka.jar -c "weka.classifiers.trees.J48"
-
-### Run 0011 => Mesure 0.544 - Params   -C 0.599603998565375 -Q 11 -O -A -J -doNotMakeSplitPointActualValue
+    model.learn(weka)
